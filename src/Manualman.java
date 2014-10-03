2bfe17e2acd7812034b0c28cc2998a4049edfaed
Charapao <pao_tia@hotmail.com>
2014-10-03 15:50:50 +0700

    Show Player and Import image

--- src/Manualman.java
+++ src/Manualman.java
@@ -3,12 +3,20 @@ import java.util.logging.Logger;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
  
 public class Manualman extends BasicGame {
-  public Manualman(String title) {
+	
+	public static float G = (float) 0.5;
+    public static final float PLAYER_JUMP_VY = 10;
+    public static final int GAME_WIDTH = 640;
+    public static final int GAME_HEIGHT = 480;
+    public static final float Ground_VX = -4;
+    static public Player player;
+	public Manualman(String title) {
 		super(title);
 		// TODO Auto-generated constructor stub
 	}
@@ -16,14 +24,30 @@ public class Manualman extends BasicGame {
 
  
   @Override
-  public void init(GameContainer gc) throws SlickException {}
+  public void init(GameContainer gc) throws SlickException { 
+	  
+	  player = new Player(GAME_WIDTH/4, 300, 0);
+  
+  
+  }
+  
  
   @Override
-  public void update(GameContainer gc, int dt) throws SlickException {}
- 
+  public void update(GameContainer gc, int dt) throws SlickException {
+  player.update();
+  }
   @Override
   public void render(GameContainer gc, Graphics g) throws SlickException {
-  g.drawString("HelloWorld",200, 200);
+	  player.render();
+	  SETCOLOR_SHAPE(g);
+  }
+  public static void SETCOLOR_SHAPE(Graphics g){
+	  g.setColor(Color.yellow);
+      g.fill(Player.circleplayer);
+      g.setColor(Color.pink);
+      g.draw(Player.circleplayer);
+	  
+	  
   }
  
   public static void main(String[] args) {
